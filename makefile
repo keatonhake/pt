ebb4d8d9fc80044a89f0b5efc20a143dd63378b6
Keaton Hake <me@keatonhake.com>
2023-02-22 17:52:08 -0500

    pt init mostly working

--- makefile
+++ makefile
@@ -1,7 +1,9 @@
 VERSION = 0.1.0
 
-PREFIX = /usr/local
+#PREFIX = /usr/local
+PREFIX = $(HOME)/.local
 MANPREFIX = $(PREFIX)/share/man
+TEMPLATES := $(PREFIX)/share/pt/templates
 
 .MAIN: default
 
@@ -10,17 +12,26 @@ default:
 	@echo 'pt is a simple tool for template based project initializing.'
 	@echo 'please edit the makefile to your liking then `make install`.'
 
+check:
+	shellcheck pt.sh
+
 
 install:
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
 	cp -f pt.sh $(DESTDIR)$(PREFIX)/bin/pt
+	sed -i 's|{{:templates}}|$(DESTDIR)$(TEMPLATES)|g' $(DESTDIR)$(PREFIX)/bin/pt
 	chmod 755 $(DESTDIR)$(PREFIX)/bin/pt
 	mkdir -p $(DESTDIR)$(MANPREFIX)/man1
 	cp -f pt.1 $(DESTDIR)$(MANPREFIX)/man1
 	chmod 644 $(DESTDIR)$(MANPREFIX)/man1/pt.1
+	mkdir -p $(DESTDIR)$(PREFIX)/share/pt
+	cp -fr templates $(DESTDIR)$(PREFIX)/share/pt
+	chmod -R u=rwX,go=rX $(DESTDIR)$(PREFIX)/share/pt
+
 
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/bin/pt \
 		$(DESTDIR)$(MANPREFIX)/man1/pt.1
+	rm -rf $(DESTDIR)$(PREFIX)/share/pt
 
-.PHONY: default install uninstall
+.PHONY: default install uninstall check
--- pt.sh
+++ pt.sh
@@ -1,53 +1,73 @@
 #!/bin/sh
 
-PROG=$(basename $0)
+PROG=$(basename "$0")
 PROJECT='{{:project}}'
+TEMPLATES='{{:templates}}'
 
 stderr() {
-  echo "$@" 1>&2
+    echo "$@" 1>&2
 }
 
 stderr2() {
-  printf "%s\n" "$@" 1>&2
+    printf "%s\n" "$@" 1>&2
 }
 
 usage() {
     stderr2 "usage: $PROG <command> [args]"
     if [ "$#" -ne 0 ]; then
-        stderr2 ""                                                 \
-        "A tool for template-based project creation"               \
-        ""                                                         \
-        "commands:"                                                \
-        "    init    Initialize a project in current directory"    \
-        "    new     Create directory and project"                 \
-        "    list    List available project templates"             \
+    stderr2 ""                                                  \
+        "A tool for template-based project creation"            \
+        ""                                                      \
+        "commands:"                                             \
+        "    init    Initialize a project in current directory" \
+        "    new     Create directory and project"              \
+        "    list    List available project templates"          \
         "    help    Show this or help for <command>"
     fi
 }
 
 get_dir() {
-    basename $(pwd)
+    basename "$(pwd)"
 }
 
 # usage: fill_template <project-name> <file>
 # replaces all instances of {{:project}} with project-name
 fill_template() {
-  sed s/"$PROJECT"/"$1"/g "$2"
+    sed -i s/"$PROJECT"/"$1"/g "$2"
 }
 
-do_init() {
-    local dir="$(get_dir)"
+list_templates() {
+    echo "templates available:"
+    for dir in "$TEMPLATES"/*/; do
+        [ -d "$dir" ] || continue
+        printf '  - %s\n' "$(basename "$dir")"
+    done
 }
 
-do_new() {
+# do_* functions are defines with () and not {} in order to
+# have some 'local' like scoping.
+
+# usage: do_init <template>
+do_init() (
+    dir="$(get_dir)"
+    [ -d "$TEMPLATES"/"$1" ] || return 1
+    cp -r "$TEMPLATES"/"$1"/* .
+
+    for file in $(find . -type f); do
+        [ -f "$file" ] || continue
+        fill_template "$dir" "$file"
+    done
+)
+
+do_new() (
     stderr2 "IMPLEMENT do_new"
-}
+)
 
-do_list() {
-    stderr2 "IMPLEMENT do_list"
-}
+do_list() (
+    list_templates
+)
 
-do_help() {
+do_help() (
     case $1 in
     init)
         ;;
@@ -58,24 +78,29 @@ do_help() {
     *)
         usage long ;;
     esac
-}
+)
 
 # usage: handle_command <cmd> [extras..]
 handle_cmd() {
-    local cmd="$1"
+    cmd="$1"
     [ "$#" -gt 1 ] && shift
 
     case $cmd in
     init)
-        do_init "$@" ;;
+        do_init "$@"
+        ;;
     new)
-        do_new "$@" ;;
+        do_new "$@"
+        ;;
     list)
-        do_list "$@" ;;
+        do_list "$@"
+        ;;
     -h|--help|help)
-        do_help "$@" ;;
+        do_help "$@"
+        ;;
     *)
-        usage;
+        usage
+        ;;
     esac
 }
 
